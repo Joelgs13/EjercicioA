21587f3c176f18eb5f82155757bd87f67479d468
Joelgs13 <050213jgonzalez@gmail.com>
2024-10-07 12:09:22 +0200

    Lista rellenada

--- src/main/java/com/example/ejercicioa/HelloApplication.java
+++ src/main/java/com/example/ejercicioa/HelloApplication.java
@@ -2,16 +2,21 @@ package com.example.ejercicioa;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class HelloApplication extends Application {
+
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("ejercicioA.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
@@ -20,4 +25,6 @@ public class HelloApplication extends Application {
     public static void main(String[] args) {
         launch();
     }
+
+
 }
